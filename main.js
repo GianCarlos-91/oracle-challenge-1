67e9d7718becb837eec70f5b4ea144fbffaabca3
Giancarlos-91 <navarro.gcn@outlook.com>
2024-08-26 20:37:51 +0000

    se añadieron los eventos de copiado

--- main.js
+++ main.js
@@ -4,17 +4,43 @@ const input = document.querySelector('#Input');
 const output = document.querySelector('#Output');
 const encryptButton = document.querySelector('#Encrypt');
 const decryptButton = document.querySelector('#Decrypt');
+const copyButton = document.querySelector('#Copy');
+const outputContainer = document.querySelector('#OutputContainer');
 
 window.enc = Encrypt;
 
+function changeHandler() {
+    if (output.value.length > 0) {
+        outputContainer.setAttribute('data', 'on');
+    } else {
+        outputContainer.setAttribute('data', 'off');
+    }
+}
+
 encryptButton.addEventListener('click', () => {
     output.value = Encrypt.encrypt(input.value);
+    changeHandler();
 });
 
 decryptButton.addEventListener('click', () => {
     output.value = Encrypt.decrypt(input.value);
+    changeHandler();
 });
 
 input.addEventListener('input', () => {
     output.value = '';
+    changeHandler();
+});
+
+let copyTimeOutID = null;
+copyButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(output.value);
+    copyButton.setAttribute('copied', 'true');
+    copyButton.textContent = 'Copiado!';
+    if (copyTimeOutID !== null) clearTimeout(copyTimeOutID);
+    copyTimeOutID = setTimeout(() => {
+        copyButton.removeAttribute('copied');
+        copyButton.textContent = 'Copiar';
+        copyTimeOutID = null;
+    }, 1000);
 });
