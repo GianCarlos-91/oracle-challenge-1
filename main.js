d2be96416f05f3010d5962546e296d139ff184ac
Giancarlos-91 <navarro.gcn@outlook.com>
2024-08-26 18:21:14 +0000

    se proporciono logica a la app

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// Add JS here
+import Encrypt from './Encrypt.js';
+
+const input = document.querySelector('#Input');
+const output = document.querySelector('#Output');
+const encryptButton = document.querySelector('#Encrypt');
+const decryptButton = document.querySelector('#Decrypt');
+
+window.enc = Encrypt;
+
+encryptButton.addEventListener('click', () => {
+    output.value = Encrypt.encrypt(input.value);
+});
+
+decryptButton.addEventListener('click', () => {
+    output.value = Encrypt.decrypt(input.value);
+});
+
+input.addEventListener('input', () => {
+    output.value = '';
+});
